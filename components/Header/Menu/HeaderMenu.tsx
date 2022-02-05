7784ceb5f2d6602ce327b19baa95125cf4d1c424
papadopan <antonios.papadopan@gmail.com>
2022-02-05 19:33:14 +0100

    feat(frontend): update structure

--- components/Header/Menu/HeaderMenu.tsx
+++ components/Header/Menu/HeaderMenu.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Menu } from 'antd'
+import { Button, Menu, Space } from 'antd'
 import {
   LogoutOutlined,
   SettingOutlined,
   UserOutlined,
 } from '@ant-design/icons'
+import { useRouter } from 'next/router'
 
-const HeaderMenu = (props) => {
-  return (
+const HeaderMenu = ({ loggedIn }) => {
+  const router = useRouter()
+  return loggedIn ? (
     <Menu title="mainmenu" selectable={false} mode="horizontal">
       <Menu.SubMenu icon={<UserOutlined />} title="Antonios" key="submenu">
         <Menu.Item icon={<UserOutlined />}>Account</Menu.Item>
@@ -17,6 +19,13 @@ const HeaderMenu = (props) => {
         <Menu.Item icon={<LogoutOutlined />}>Logout</Menu.Item>
       </Menu.SubMenu>
     </Menu>
+  ) : (
+    <Space>
+      <Button onClick={() => router.push('login')}>Login</Button>
+      <Button type="primary" onClick={() => router.push('/register')}>
+        Sign Up
+      </Button>
+    </Space>
   )
 }
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Layout } from 'antd'
+import Header from '../components/Header'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Layout style={{ minHeight: '100vh' }}>
+      <Layout.Header>
+        <Header />
+      </Layout.Header>
+      <Layout.Content>
+        <Component {...pageProps} />
+      </Layout.Content>
+      <Layout.Footer>antonios</Layout.Footer>
+    </Layout>
+  )
 }
 
 export default MyApp
