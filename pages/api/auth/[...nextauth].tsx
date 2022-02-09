2c40a3d091e75beaac3c4f8c935d2401895dc740
papadopan <antonios.papadopan@gmail.com>
2022-02-09 17:01:25 +0100

    feat(auth): add google login

--- pages/api/auth/[...nextauth].tsx
+++ pages/api/auth/[...nextauth].tsx
@@ -1,12 +1,21 @@
 import NextAuth from 'next-auth'
 import CredentialsProvider from 'next-auth/providers/credentials'
+import GoogleProvider from 'next-auth/providers/google'
 
 export default NextAuth({
-  session: { strategy: 'jwt' },
+  secret: process.env.NEXT_PUBLIC_JWT_SECRET,
+  jwt: {
+    secret: process.env.NEXT_PUBLIC_JWT_SECRET,
+  },
   providers: [
+    GoogleProvider({
+      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET!,
+    }),
     CredentialsProvider({
-      async authorize(credentials, req) {
-        return { email: credentials.email }
+      authorize(credentials) {
+        if (credentials.email) return { email: credentials.email }
+        return null
       },
     }),
   ],
